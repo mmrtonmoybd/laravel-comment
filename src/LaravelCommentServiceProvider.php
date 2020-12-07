fd767bfdc7791b656306434c9890fb470f25fb4f
mmrtonmoybd <rmedha037@gmail.com>
2020-12-07 14:43:25 +0600

    first commit

---
+++ src/LaravelCommentServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+namespace Mmrtonmoybd\Comment;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCommentServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+}
